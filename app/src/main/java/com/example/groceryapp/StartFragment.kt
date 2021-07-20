41a6153743e8d243676ed6ed3c03b0c1e0cac7b1
tiennh <dakietkhac007@gmail.com>
2021-07-20 16:17:29 +0700

    commit

--- app/src/main/java/com/example/groceryapp/StartFragment.kt
+++ app/src/main/java/com/example/groceryapp/StartFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_start.*
+
 
 
 class StartFragment : Fragment() {
@@ -18,9 +20,21 @@ class StartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_start, container, false)
+
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_sign_in.setOnClickListener {
+
+        }
+
+        btn_sign_up.setOnClickListener {
+
+        }
+    }
 
 }
